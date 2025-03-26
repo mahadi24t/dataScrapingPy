8718470dbb1b2c801c37db0b83b36b391329fb91
mahadi24t <mahadi24t@outlook.com>
2025-03-26 15:32:22 +0600

    scrapingwithbeautifulsoup

---
+++ webscraping.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
+    # print(content)
+    
+    soup = BeautifulSoup(content, 'lxml')
+    # print(soup.prettify())
+    
+    tags = soup.find_all('h5')
+    print(tags)
+    
+    
