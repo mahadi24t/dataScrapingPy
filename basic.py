5c6f7754c76246442741553d07c9ad6ac4bc6d78
mahadi24t <mahadi24t@outlook.com>
2025-04-10 19:38:45 +0600

    basicpy

---
+++ basic.py
@@ -0,0 +1,18 @@
+states = ["California", "Navada" ,  " Texas", "New York"]
+
+#print(states)
+
+for state in states:
+    print(state)
+    
+for navada in states:
+    if navada == "Navada":
+        print(navada)
+        
+
+with open('text.txt','w') as file:
+    file.write('Hello, world!')
+
+
+
+
