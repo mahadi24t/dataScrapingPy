847b7d95ff9a9737253f4a5a5d324e3f4f12eae7
mahadi24t <mahadi24t@outlook.com>
2025-04-29 19:09:58 +0600

    userinputexercise

---
+++ userinputexcercise.py
@@ -0,0 +1,4 @@
+fname=input("Enter your name:")
+dinkm=input("Enter distance covered in km:")
+mile= float(dinkm)*1.609
+print("Hello!"+ fname +" Greetings for coming Here! "+ "You have covered "+f'{mile}'+ ' miles.'  )
