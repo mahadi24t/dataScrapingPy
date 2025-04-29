f2c967980d92997f3dd98803112f5b243abf7c4a
mahadi24t <mahadi24t@outlook.com>
2025-04-29 18:18:58 +0600

    string_formatting

---
+++ stringformatting.py
@@ -0,0 +1,8 @@
+name='TERRY'
+color = 'RED'
+msg = '[' + name + '] loves the color ' + color.lower() + '!'
+msg1 = f'[{name}] loves the color {color.lower()}!'
+msg2 = f'[{name.capitalize()}] loves the color {color.upper()}!'
+print(msg)
+print(msg1)
+print(msg2)
